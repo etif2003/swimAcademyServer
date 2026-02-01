25697164c61dd00c7c1d7478d373b2f33616098e
etif2003 <etib2003@gmail.com>
2026-02-01 14:34:58 +0200

    Update User.js
    Updated User

--- models/User.js
+++ models/User.js
@@ -2,17 +2,51 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    password: { type: String, required: true }, // בשרת תצפין עם bcrypt
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: /.+\@.+\..+/,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
     role: {
       type: String,
       enum: ["student", "instructor", "school", "admin"],
       default: "student",
     },
+
+    status: {
+      type: String,
+      enum: ["active", "blocked"],
+      default: "active",
+    },
+
+    image: {
+      type: String,
+      trim: true,
+    },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 export const User = mongoose.model("User", userSchema);
